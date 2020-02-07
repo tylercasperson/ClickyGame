10055dc9a680d82bf77ab90d0e2262a3f754a516
Tyler Casperson <tylercasperson@gmail.com>
2020-02-07 01:32:23 -0700

    changed border styles

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -3,11 +3,11 @@ import './style.css';
 
 function NavBar(props) {
   return (
-    <nav className="nav justify-content-center col-md-12">
-        <div className="col-md-1">Clicky Game</div>
+    <nav className="nav justify-content-center col-md-12 position-fixed">
+        <div className="col-md-4 title">Natures charm</div>
         <div className="col-md-4 track"></div>
-        <div>Score: {props.score}</div>
-        <div className="col-md-2">Best: {props.best}</div>
+        <div className="score">Score: {props.score}</div>
+        <div className="col-md-2 best">Best: {props.best}</div>
     </nav>
   );
 }
