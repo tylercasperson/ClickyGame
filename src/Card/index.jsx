ff38682ba6d3108e4cd74d9e4628858e9651c758
Tyler Casperson <tylercasperson@gmail.com>
2020-02-08 05:52:25 -0700

    github deployable

--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './style.css';
 
-
-// let imageArray = [picture1, picture2];
-// let randomImage = Math.floor(Math.random()*imageArray.length);
-
 function Picture(props) {
   return (
     <button className="card border-primary mb-3 border-dark pictureCard" onClick={props.onClick}>
